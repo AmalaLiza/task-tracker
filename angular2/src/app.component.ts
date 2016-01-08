8241487220cea77d3ad9803122553df81ddcd774
Arjun U <ar5junu@gmail.com>
2016-01-08 21:08:49 +0530

    Setup Angular 2, Webpack, TS

---
+++ angular2/src/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+console.log("app comp");
+
+@Component({
+    selector: 'app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
---
+++ angular2/src/vendor.ts
@@ -0,0 +1,6 @@
+import 'reflect-metadata';
+
+import 'angular2/platform/browser';
+import 'angular2/core';
+import 'angular2/http';
+import 'angular2/router';
