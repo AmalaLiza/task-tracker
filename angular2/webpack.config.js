4e0d32683024f2bd96f957401da4a019ae898bf1
Arjun U <arjunu@users.noreply.github.com>
2016-01-11 12:06:50 +0530

    Update webpack.config.js

--- angular2/webpack.config.js
+++ angular2/webpack.config.js
@@ -1,10 +1,40 @@
-angular2
-dist
-node_modules
-src
-typings
-index.html
-package.json
-server.js
-tsconfig.json
-webpack.config.js
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'eval',
+    entry: {
+        'app': [ // For hot style updates
+            'webpack/hot/dev-server',
+            // The script refreshing the browser on none hot updates
+            'webpack-dev-server/client?http://localhost:8001',
+            './src/boot.ts'
+        ],
+        'vendor': './src/vendor.ts'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                loader: 'style!css!sass'
+            },
+            {
+                test: /\.ts/,
+                exclude: /node_modules/,
+                loader: 'ts-loader',
+            }
+        ],
+        include: path.join(__dirname, 'src')
+    }
+};
