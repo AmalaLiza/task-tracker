1deefd9fd477bb27ed9e03a0d79c2c7060bc5a55
ajayvarghese003 <ajay.v@razorthink.net>
2016-01-13 14:42:53 +0530

    Modified footer progressbar

--- react/src/components/progress-bar/progress-bar.tsx
+++ react/src/components/progress-bar/progress-bar.tsx
@@ -5,11 +5,8 @@ export default class ProgressBar extends React.Component<any, any> {
 
     render(){
         return <div className="progress-bar-wrapper fleft">
-                    <div className="progress-bar-title">
-                        <span>Create and Upload Razorthink website repositories</span>
-                    </div>
                     <div className="progress-bar">
-                        <div className="progress-bar__progress" style="width:30%"></div>
+                        <div className="progress-bar__progress" style={{width:"30%"}}></div>
                     </div>
                 </div>
     }
--- react/src/components/task-tracker/task-tracker.tsx
+++ react/src/components/task-tracker/task-tracker.tsx
@@ -13,16 +13,17 @@ export default class TaskTracker extends React.Component<TaskTrackerProps, any> 
                 <a href="javascript:void(0)" className="play-ico fleft"></a>
                 <div className="progress-wrapper fleft clearfix">
 
-                    <div className="time-indicator fleft">11:30:23</div>
-                    <div className="progress-bar-wrapper fleft">
-                        <div className="progress-bar-title">
-                            <span>Create and Upload Razorthink website repositories</span>
-                        </div>
-                        <div className="progress-bar">
-                            <div className="progress-bar__progress" style={{width:'30%'}}></div>
+                    <div className="progress-bar-title">
+                        <span>Create and Upload Razorthink website repositories</span>
+                    </div>
+                    <div className="progress-bar-wrapper">
+                        <div className="time-indicator fleft">11:30:23</div>
+                        <div className="time-indicator fright">11:30:23</div>
+
+                        <div className="progress-bar fleft">
+                            <div className="progress-bar__progress fleft" style={{width:'30%'}}></div>
                         </div>
                     </div>
-                    <div className="time-indicator fright">11:30:23</div>
 
                 </div>
             </div>
