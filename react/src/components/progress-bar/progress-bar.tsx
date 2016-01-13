24834a72e37b58bfc205a64c44d2f225c8680547
ajayvarghese003 <ajay.v@razorthink.net>
2016-01-13 17:30:05 +0530

    Updated Progress Bar

--- react/src/components/progress-bar/progress-bar.tsx
+++ react/src/components/progress-bar/progress-bar.tsx
@@ -12,7 +12,7 @@ export default class ProgressBar extends React.Component<ProgressBarProps, any> 
     }
 
     render() {
-        return <div className="progress-bar-wrapper">
+        return <div className="progress-bar-wrapper clearfix width-100per">
             <div className="time-indicator fleft">11:30:23</div>
             <div className="time-indicator fright">11:30:23</div>
 
