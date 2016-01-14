0a0cf8da06b6250a5be18d207c893d81fcf5799b
Arjun U <arjunu@users.noreply.github.com>
2016-01-14 14:44:43 +0530

    Create gulpfile.js

---
+++ angular2/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    del = require('del');
+
+var paths = [
+    "./src/global.scss",
+    "./src/**/*.scss"
+];
+
+var cssPaths = [
+    "./src/global.css",
+    "./src/**/*.css"
+];
+
+gulp.task('sass', function () {
+    return gulp.src(paths, {base: "./"})
+        .pipe(sass())
+        .pipe(gulp.dest("./"));
+});
+
+gulp.task('del-css', function(cb){
+    del(cssPaths, cb);
+});
